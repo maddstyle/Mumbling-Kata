77cef849b66311b71fe8961b01166d623c755da5
Michaela DeCamara <56228050+maddstyle@users.noreply.github.com>
2019-10-13 22:07:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-https://www.codewars.com/kata/mumbling/train/javascript
+function accum(s) {
+	// your code
+  let totalString = "";
+  let characters = s.split("");
+
+  for (let i = 0; i < characters.length; i++) {
+  
+    let characterSubString = "";
+    
+      for (let j = 0; j < (i + 1); j++){
+      
+        let letterToAdd = "";
+        if(j === 0) {
+          letterToAdd = characters[i].toUpperCase();
+        } else {
+          letterToAdd = characters[i].toLowerCase();
+        }
+        
+        characterSubString += letterToAdd;
+        
+      }
+      
+      totalString += characterSubString + "-";
+      
+  }
+    totalString = totalString.slice(0, -1);
+    return totalString;
+    
+}
